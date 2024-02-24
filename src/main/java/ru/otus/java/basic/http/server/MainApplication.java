4832246bccceb5d5420626c0d0eb52c82025a3fd
vitaliy <vitalily@mail.com>
2024-02-24 18:22:24 +0700

    init

---
+++ src/main/java/ru/otus/java/basic/http/server/MainApplication.java
@@ -0,0 +1,9 @@
+package ru.otus.java.basic.http.server;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        HttpServer server = new HttpServer(Integer.parseInt((String)System.getProperties().getOrDefault("port", "8189")));
+        server.start();
+    }
+}
+
